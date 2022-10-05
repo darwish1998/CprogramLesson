54d1d571f7b8f67c141568b56c06e34e7ea17c85
darwish1998 <55480333+darwish1998@users.noreply.github.com>
2022-10-05 09:44:16 +0300

    introduction to C programming

---
+++ myCprogram.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  
+  int mynum =  -1;
+  if (mynum > 1) {
+  	printf("Number is positive");
+    
+   
+  }
+  else if (mynum < 0)
+  {
+  	printf("is is neg\n");
+    
+  }
+  else {
+  
+  	printf("Zero");
+  }
+  
+  int time = 30;
+  (time <12) ? printf("Good day") : printf("Good evening");
+  
+
+  
+}
